9da90fb1c562365e3472fd466ae855af41a1caf7
Andrii Kachkarov <Andriy.Kachkarov@gmail.com>
2021-01-04 10:37:41 +0200

    fix scrolling behavior when popup opens

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -5,6 +5,7 @@ const modals = () => {
         const modal = document.querySelector(modalSelector);
         const close = document.querySelector(closeSelector);
         const windows = document.querySelectorAll('[data-modal]');
+        const scroll = calcScroll();
 
         trigger.forEach(item => {
             item.addEventListener('click', (e) => {
@@ -16,6 +17,7 @@ const modals = () => {
 
                 modal.style.display = 'block';
                 document.body.classList.add('modal-open');
+                document.body.style.marginRight = scroll + 'px';
             });
         });
 
@@ -24,7 +26,7 @@ const modals = () => {
 
             modal.style.display = 'none';
             document.body.classList.remove('modal-open');
-
+            document.body.style.marginRight = '0px';
         });
 
         modal.addEventListener('click', (e) => {
@@ -33,7 +35,7 @@ const modals = () => {
 
                 modal.style.display = 'none';
                 document.body.classList.remove('modal-open');
-
+                document.body.style.marginRight = '0px';
             }
         })
     }
@@ -49,6 +51,20 @@ const modals = () => {
         windows.forEach(item => {
             item.style.display = 'none';
         });
+    }
+
+    function calcScroll() {
+        let div = document.createElement('div');
+
+        div.style.width = '50px';
+        div.style.height = '50px';
+        div.style.overflowY = 'scroll';
+        div.style.visibility = 'hidden';
+
+        document.body.appendChild(div);
+        let scrollWidth = div.offsetWidth - div.clientWidth;
+        div.remove();
+        return scrollWidth;
     }
 
 
